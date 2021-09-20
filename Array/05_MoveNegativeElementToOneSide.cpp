fdd2f4c87c07f5d48a44ced5d28d84c6cbe345ff
Mohit <77422525+Mohit-0607@users.noreply.github.com>
2021-09-20 22:19:45 +0530

    Update and rename Move_-ve_element_to_one_side.c to 05_MoveNegativeElementToOneSide.cpp

---
+++ Array/05_MoveNegativeElementToOneSide.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int pivot = 0;
+    int i = 0;
+    int j = n-1;
+    int temp;
+    for(int x=0;x<n;x++){
+        while(arr[i]>pivot)
+            i++;
+        while(arr[j]<pivot)
+            j--;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for(int x=0;x<n;x++)
+        cout<<arr[x]<<" ";
+    return 0;
+}
