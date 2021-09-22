4c7c09586597cdf285def8d943856d90fa9c1c34
Mohit <77422525+Mohit-0607@users.noreply.github.com>
2021-09-22 21:22:57 +0530

    Create 01_CreateAndPrintAGraphUsingAdjacencyMatrix.cpp

--- Graph/01_CreateAndPrintAGraphUsingAdjacencyMatrix.cpp
+++ Graph/01_CreateAndPrintAGraphUsingAdjacencyMatrix.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int main(){
-    int v;
-    cin>>v;
+    int v,e;
+    cin>>v>>e;
     int arr[v+1][v+1];
     for(int i=1;i<=v;i++){
         for(int j=1;j<=v;j++){
             arr[i][j]=0;
         }
     }
-    for(int i=1;i<=v;i++){
+    for(int i=0;i<e;i++){
         int x,y;
         cin>>x>>y;
-        for(int j=1;j<=v;j++){
-            arr[x][y]=1;
-            arr[y][x]=1;
-        }
-    }
+        arr[x][y]=1;
+        arr[y][x]=1;
+    } 
      for(int i=1;i<=v;i++){
         cout<<i<<"-->";
         for(int j=1;j<=v;j++){
