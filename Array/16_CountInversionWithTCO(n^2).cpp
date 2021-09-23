d461a9286890bc286491deb29e4f55734fe95365
Mohit <77422525+Mohit-0607@users.noreply.github.com>
2021-09-23 19:39:13 +0530

    Create 16_CountInversionWithTCO(n^2).cpp

---
+++ Array/16_CountInversionWithTCO(n^2).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int count = 0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[i]>arr[j])   
+                count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
